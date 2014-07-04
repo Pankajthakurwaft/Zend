b5d52ebc4f28cdec9b9dcf7373ddbf5f65e5949b
sarostech <wafttech@gmail.com>
2014-07-04 15:38:36 +0530

    Update module.config.php

--- module.config.php
+++ module.config.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Zend Framework (http://framework.zend.com/)
- *
+ *dsvfsdafsdafsfsdfs
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
+sdfsdafsdf
 return array(
     'router' => array(
         'routes' => array(
